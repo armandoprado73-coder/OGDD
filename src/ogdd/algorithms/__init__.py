57c5e67734e36af539b86e0d3fe1ec49a3e7db20
armandoprado73-coder <armandoprado73@gmail.com>
2026-07-18 21:27:56 -0600

    Create __init__.py

---
+++ src/ogdd/algorithms/__init__.py
@@ -0,0 +1,17 @@
+"""
+OGDD Algorithms Module
+
+Contains computational algorithms for
+geometric and dental analysis.
+
+Algorithms are built on top of the
+OGDD geometry foundation.
+"""
+
+from __future__ import annotations
+
+
+__all__ = [
+    "PCAResult",
+    "PrincipalComponentAnalysis",
+]
