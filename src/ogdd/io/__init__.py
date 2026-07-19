69558117ee412592c51f693abcb53a4ec3aa6d4c
armandoprado73-coder <armandoprado73@gmail.com>
2026-07-18 20:50:50 -0600

    Create __init__.py

---
+++ src/ogdd/io/__init__.py
@@ -0,0 +1,27 @@
+"""
+OGDD Input / Output Module
+
+This package provides tools to import and export
+3D geometry data.
+
+Supported formats will include:
+
+- STL
+- OBJ
+- PLY
+
+All readers convert external data into
+the internal OGDD Mesh representation.
+"""
+
+from __future__ import annotations
+
+
+__all__ = [
+    "STLReader",
+]
+
+
+# Future imports:
+
+# from .stl import STLReader
